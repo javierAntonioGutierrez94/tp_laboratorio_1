da250f685fd05ac0002182e3036e92f9eb8872b8
javierAntonioGutierrez94 <52746811+javierAntonioGutierrez94@users.noreply.github.com>
2019-09-02 20:40:53 -0300

    Update miBiblioteca.h

--- tp1/miBiblioteca.h
+++ tp1/miBiblioteca.h
@@ -3,7 +3,7 @@ int suma(int op1, int op2);
 int resta(int op1, int op2);
 int multiplicacion(int op1, int op2);
 int division(int op1, int op2);
-int factorial(int n);
+int factorial(int valor);
 
 
  // MIBIBLIOTECA_H_INCLUDED
