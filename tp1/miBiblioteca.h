79a2e1f0e39cb90a9a70496ab2a834f2c6a7050f
javierAntonioGutierrez94 <52746811+javierAntonioGutierrez94@users.noreply.github.com>
2019-09-02 21:57:20 -0300

    Update miBiblioteca.h

--- tp1/miBiblioteca.h
+++ tp1/miBiblioteca.h
@@ -1,9 +1,43 @@
-
+/**
+*\brief Realiza la suma de dos operadores
+*\param op1: un numero entero
+*\param op2: un numero entero
+*\return retorna el resultado de la suma en enteros
+*
+*/
 int suma(int op1, int op2);
+
+/**
+*\brief Realiza la resta de dos operadores
+*\param op1 es un numero entero
+*\param op2 es un numero entero
+*\return retorna el resultado de la resta de op1-op2 en enteros
+*
+*/
 int resta(int op1, int op2);
+
+/**
+*\brief Realiza la multiplicacion de dos operadores
+*\param op1 es un numero entero
+*\param op2 es un numero entero
+*\return retorna el resultado de la multiplicacion en enteros
+*
+*/
 int multiplicacion(int op1, int op2);
+
+/**
+*\brief Realiza la division de dos operadores
+*\param op1 es un numero entero
+*\param op2 es un numero entero
+*\return retorna el resultado de la divison "casteada" en decimales (Float);
+*
+*/
 int division(int op1, int op2);
+
+/**
+*\brief Realiza el factorial de un operador
+*\param op1 es un numero entero
+*\return retorna el factorial de el numero cargado;
+*
+*/
 int factorial(int valor);
-
-
- // MIBIBLIOTECA_H_INCLUDED
